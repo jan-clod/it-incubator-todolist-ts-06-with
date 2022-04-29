d583a73fc770023f355df3b0859fc790a90e98e3
JanClod <fia.yan@mail.ru>
2022-04-29 12:30:11 +0300

    Updates

---
+++ precache-manifest.5139cac8c7f832d1b924f9d0d43c43d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8eeb91a5947f864d5233bcb26fc3356",
+    "url": "/it-incubator-todolist-ts/index.html"
+  },
+  {
+    "revision": "9941dc64b40f9f9043e3",
+    "url": "/it-incubator-todolist-ts/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "11930786429232958006",
+    "url": "/it-incubator-todolist-ts/static/js/2.83ec6f1e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/it-incubator-todolist-ts/static/js/2.83ec6f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9941dc64b40f9f9043e3",
+    "url": "/it-incubator-todolist-ts/static/js/main.e808efd4.chunk.js"
+  },
+  {
+    "revision": "1ab7487ce92f6fb728b6",
+    "url": "/it-incubator-todolist-ts/static/js/runtime-main.f8216a61.js"
+  }
+]);
